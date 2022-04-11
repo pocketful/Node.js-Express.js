6f181099e7b9df74a785d98ce92857f65dd9834b
pocketful <iveta.jac@gmail.com>
2022-04-11 18:30:09 +0300

    dynamic route, cars, cao task1

--- Node-4-Dynamic-Routes/src/server.js
+++ Node-4-Dynamic-Routes/src/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const { response } = require('express');
 const app = express();
 const PORT = 3000;
 
