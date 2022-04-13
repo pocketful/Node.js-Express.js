150f55dae3d5aa2890a76c30897702383ea2dcdc
pocketful <iveta.jac@gmail.com>
2022-04-13 11:05:52 +0300

    connect mongodb users

--- Node-5-MongoDB/src/api/users.js
+++ Node-5-MongoDB/src/api/users.js
@@ -1,9 +1,23 @@
+require('dotenv').config();
 const express = require('express');
+const { MongoClient } = require('mongodb');
 
 const usersRouter = express.Router();
 
-usersRouter.get('/users', (req, res) => {
-  res.json('GET /users route');
+const dbClient = new MongoClient(process.env.MONGO_DB_STRING);
+
+usersRouter.get('/users', async (req, res) => {
+  try {
+    await dbClient.connect();
+    console.log('connection opened');
+    res.json('connection ok');
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json('something went wrong');
+  } finally {
+    await dbClient.close();
+    console.log('connection closed');
+  }
 });
 
 module.exports = usersRouter;
