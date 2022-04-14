30c5a61755a6fcb946f3f7d8671bc18a8b7f8b8a
pocketful <iveta.jac@gmail.com>
2022-04-14 21:32:26 +0300

    dynamic find queries

--- Node-5-MongoDB/model/usersModel.js
+++ Node-5-MongoDB/model/usersModel.js
@@ -39,12 +39,11 @@ async function findFemalesDb() {
   }
 }
 
-// galima daryti findDb, bet vis tiek pacia paieska palikti findFemalesDb(), findMalesDb()
-async function findDb(queryValue) {
+// galima daryti getArrDb(query), bet vis tiek findFemalesDb(), findMalesDb() grazina getArrDB. Kai query sudetingas. Kai norim irasyt tada postToDb, ar deleteFromDb
+async function findDb(query) {
   console.log('findDb ran');
   try {
     await dbClient.connect();
-    const query = queryValue;
     console.log('connection opened');
     const resource = dbClient.db(dbName).collection(collName);
     const arr = await resource.find(query).toArray();
@@ -58,8 +57,66 @@ async function findDb(queryValue) {
   }
 }
 
+async function findUserByAge(age) {
+  console.log('findUserByEmail ran');
+  try {
+    await dbClient.connect();
+    const query = { age };
+    console.log('connection opened');
+    const resource = dbClient.db(dbName).collection(collName);
+    const userByAge = await resource.find(query).toArray();
+    return userByAge;
+  } catch (error) {
+    console.log(error.message);
+    return false;
+  } finally {
+    await dbClient.close();
+    console.log('connection closed');
+  }
+}
+
+async function findUserByEmail(email) {
+  console.log('findUserByEmail ran');
+  try {
+    await dbClient.connect();
+    const query = { email };
+    console.log('connection opened');
+    const resource = dbClient.db(dbName).collection(collName);
+    const userByEmail = await resource.find(query).toArray();
+    return userByEmail;
+  } catch (error) {
+    console.log(error.message);
+    return false;
+  } finally {
+    await dbClient.close();
+    console.log('connection closed');
+  }
+}
+
+async function findUsersByEmails(emailsArr) {
+  console.log('findUserByEmail ran');
+  try {
+    await dbClient.connect();
+    const query = { email: { $in: emailsArr } };
+    // db.users.find({age: {$in: [22, 28]}})
+    console.log('connection opened');
+    const resource = dbClient.db(dbName).collection(collName);
+    const usersByEmails = await resource.find(query).toArray();
+    return usersByEmails;
+  } catch (error) {
+    console.log(error.message);
+    return false;
+  } finally {
+    await dbClient.close();
+    console.log('connection closed');
+  }
+}
+
 module.exports = {
   findMalesDb,
   findFemalesDb,
   findDb,
+  findUserByAge,
+  findUserByEmail,
+  findUsersByEmails,
 };
--- Node-5-MongoDB/src/api/users.js
+++ Node-5-MongoDB/src/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 // const { malesIndex } = require('../controller/usersController');
-const { findMalesDb, findDb } = require('../../model/usersModel');
+const { findMalesDb, findDb, findUserByEmail, findUsersByEmails, findUserByAge } = require('../../model/usersModel');
 const { dbClient } = require('../config');
 
 const usersRouter = express.Router();
@@ -57,9 +57,9 @@ async function hascarController(req, res) {
     const query = { hasCar: true };
     console.log('connection opened');
     const resource = dbClient.db(dbName).collection(collName);
-    const hasCarArr = await resource.find(query).toArray();
-    console.log(hasCarArr);
-    res.json(hasCarArr);
+    const hascarArr = await resource.find(query).toArray();
+    console.log(hascarArr);
+    res.json(hascarArr);
   } catch (error) {
     console.log(error.message);
     res.status(500).json('something went wrong');
@@ -68,7 +68,7 @@ async function hascarController(req, res) {
     console.log('connection closed');
   }
 }
-// usersRouter.get('/users/males', malesIndex) - kai su controller
+// usersRouter.get('/users/males', malesIndex) - kai su controller, tai tik sita eilute
 // GET /api/users/males - atrenkam tik vyrus
 usersRouter.get('/users/males', async (req, res) => {
   console.log('usersRouter.get(/users/males) ran');
@@ -99,12 +99,52 @@ usersRouter.get('/users/males', async (req, res) => {
 // GET /api/users/females - atrenkam tik moteris
 usersRouter.get('/users/females', async (req, res) => {
   console.log('usersRouter.get(/users/females) ran');
-  const femalesArr = await findDb({ gender: 'female' });
+  const query = { gender: 'female' };
+  const femalesArr = await findDb(query);
   if (femalesArr === false) {
     res.status(500).json('something went wrong');
     return;
   }
   res.json(femalesArr);
+});
+
+// GET /api/users/age/gt/20 - atrenkam zmones vyresnius nei 20 (20 dinaminis segmentas kuriam galim paduoti koki norim skaiciu)
+usersRouter.get('/users/age/gt/:someAge', async (req, res) => {
+  console.log('usersRouter.get(/users/age/gt/:someAge) ran');
+  const age = +req.params.someAge;
+  console.log('age ===', age);
+  const userByAge = await findUserByAge(age);
+  if (userByAge === false) {
+    res.status(500).json('something went wrong');
+    return;
+  }
+  res.json(userByAge);
+});
+
+// GET /api/users/email/test6@test.com - atrenkam useri pagal email (dynamic)
+usersRouter.get('/users/email/:someEmail', async (req, res) => {
+  console.log('usersRouter.get(/users/someEmail/) ran');
+  const email = req.params.someEmail;
+  console.log('email ===', email);
+  const userByEmail = await findUserByEmail(email);
+  if (userByEmail === false) {
+    res.status(500).json('something went wrong');
+    return;
+  }
+  res.json(userByEmail);
+});
+
+// GET /api/users/email/test6@test.com,test3@test.com - atrenkam kelis userius pagal email, surasytus per kableli (dynamic)
+usersRouter.get('/users/email/:someEmails', async (req, res) => {
+  console.log('usersRouter.get(/users/someEmails/) ran');
+  const emailsArr = req.params.someEmails.split(',');
+  console.log('emailsArr ===', emailsArr);
+  const usersByEmails = await findUsersByEmails(emailsArr);
+  if (usersByEmails === false) {
+    res.status(500).json('something went wrong');
+    return;
+  }
+  res.json(usersByEmails);
 });
 
 module.exports = usersRouter;
