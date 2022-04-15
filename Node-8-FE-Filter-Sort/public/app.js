3cae52652d02dc36ee44ff3b39bc956ac94929a9
pocketful <iveta.jac@gmail.com>
2022-04-15 11:16:33 +0300

    sort by age

--- Node-8-FE-Filter-Sort/public/app.js
+++ Node-8-FE-Filter-Sort/public/app.js
@@ -1,8 +1,11 @@
 const URL = 'http://localhost:3000/api';
+const outputEl = document.getElementById('output');
+const sortAgeBtn = document.getElementById('sortAge');
 
 function renderToHtml(array) {
+  outputEl.innerHTML = '';
   const ulEl = document.createElement('ul');
-  document.body.append(ulEl);
+  outputEl.append(ulEl);
   array.forEach((user) => {
     const liEl = document.createElement('li');
     liEl.textContent = `Email: ${user.email} | Age: ${user.age} | Gender: ${user.gender} | Has car: ${user.hasCar} | City: ${user.city}`;
@@ -10,15 +13,19 @@ function renderToHtml(array) {
   });
 }
 
-async function getData() {
-  const response = await fetch(`${URL}/users`);
-  // console.log(response);
+async function getData(urlEnd) {
+  const response = await fetch(`${URL}/${urlEnd}`);
   if (response.ok) {
     const data = await response.json();
-    console.log('data', data);
+    // console.log('data', data);
     renderToHtml(data);
   } else {
     console.log('error');
   }
 }
-getData();
+getData('users');
+
+sortAgeBtn.addEventListener('click', () => {
+  getData('users/sort-age');
+//   console.log('sorting');
+});
--- Node-8-FE-Filter-Sort/src/api/usersRouter.js
+++ Node-8-FE-Filter-Sort/src/api/usersRouter.js
@@ -24,4 +24,28 @@ usersRouter.get('/users', async (req, res) => {
   }
 });
 
+usersRouter.get('/users/sort-age', async (req, res) => {
+  try {
+    const options = {
+      sort: { age: 1 },
+    };
+    await dbClient.connect();
+    console.log('connection opened');
+    // get all users in json format []
+    const resource = dbClient.db(dbName).collection(collName);
+    const usersArr = await resource.find({}, options).toArray();
+    console.log(usersArr);
+    res.json(usersArr);
+  } catch (error) {
+    console.error('error in get users', error);
+    res.status(500).json('something went wrong');
+  } finally {
+    await dbClient.close();
+    console.log('connection closed');
+  }
+});
+
+
+
+
 module.exports = usersRouter;
