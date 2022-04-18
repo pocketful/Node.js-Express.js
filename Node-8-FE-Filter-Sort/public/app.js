ae38ba608f5d6e94eab175d6f47e6fb56f78c191
pocketful <iveta.jac@gmail.com>
2022-04-18 15:57:03 +0300

    renaming, renderUsers display, eslint, btns

--- Node-8-FE-Filter-Sort/public/app.js
+++ Node-8-FE-Filter-Sort/public/app.js
@@ -1,37 +1,54 @@
 const URL = 'http://localhost:3000/api';
 const outputEl = document.getElementById('output');
-const sortAgeBtn = document.getElementById('sortAge');
+const sortAgeBtn = document.getElementById('sort-age');
 
 let sortOrder = 'DESC';
 const orderDisplayEl = sortAgeBtn.querySelector('span');
 orderDisplayEl.textContent = sortOrder;
 
-function renderToHtml(array) {
+function renderUsers(usersArr) {
   outputEl.innerHTML = '';
   const ulEl = document.createElement('ul');
   outputEl.append(ulEl);
-  array.forEach((user) => {
+  usersArr.forEach((userObj) => {
     const liEl = document.createElement('li');
-    liEl.textContent = `Email: ${user.email} | Age: ${user.age} | Gender: ${user.gender} | Has car: ${user.hasCar} | City: ${user.city}`;
+    liEl.textContent = `${userObj.name} is ${userObj.age} years old 
+    ${userObj.gender}${userObj.isStudent ? ' student' : ''}.`;
     ulEl.append(liEl);
   });
 }
 
-async function getData(urlEnd) {
+async function getUsers(urlEnd) {
   const response = await fetch(`${URL}/${urlEnd}`);
   if (response.ok) {
     const data = await response.json();
     // console.log('data', data);
-    renderToHtml(data);
+    renderUsers(data);
   } else {
     console.log('error');
   }
 }
-getData('users/sort-age/ASC');
+getUsers('users/sort-age/ASC');
 
 sortAgeBtn.addEventListener('click', () => {
-  getData(`users/sort-age/${sortOrder}`);
+  getUsers(`users/sort-age/${sortOrder}`);
   sortOrder = sortOrder === 'DESC' ? 'ASC' : 'DESC';
   orderDisplayEl.textContent = sortOrder;
   console.log('sorting');
 });
+
+// /* <ol id="list"></ol>
+// const listEl = document.getElementById('list'); */
+// function renderUsers2(usersArr) {
+//   const listString = usersArr
+//     .map(
+//       (uObj) => `
+//       <li>
+//       <p><strong>${uObj.name}</strong> Is ${uObj.age} year old
+//       ${uObj.isStudent ? 'student' : ''}. <i>Gender:</i> ${uObj.gender}</p>
+//       </li>
+//   `,
+//     )
+//     .join('');
+//   listEl.innerHTML = listString;
+// }
