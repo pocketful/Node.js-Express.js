21d2601eaed207f924d62da59fa47bd8ac1926fb
pocketful <iveta.jac@gmail.com>
2022-04-18 19:13:52 +0300

    post new pet

--- Node-8-FE-Filter-Sort-Cao/src/api/petsRouter.js
+++ Node-8-FE-Filter-Sort-Cao/src/api/petsRouter.js
@@ -23,6 +23,25 @@ petsRouter.get('/pets', async (req, res) => {
   }
 });
 
+// POST
+petsRouter.post('/pets', async (req, res) => {
+  try {
+    await dbClient.connect();
+    console.log('connection opened');
+    const newPostObj = req.body;
+    const resource = dbClient.db(dbName).collection(collName);
+    const insertResult = await resource.insertOne(newPostObj);
+    console.log('insertResult ===', insertResult);
+    res.json(insertResult);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json('something went wrong');
+  } finally {
+    await dbClient.close();
+    console.log('connection closed');
+  }
+});
+
 module.exports = {
   petsRouter,
 };
