4e9cbda63e4adb32b17f4f14890ee51829a30c81
pocketful <iveta.jac@gmail.com>
2022-04-18 18:59:11 +0300

    get pets from db

--- Node-8-FE-Filter-Sort-Cao/src/server.js
+++ Node-8-FE-Filter-Sort-Cao/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const { PORT } = require('./config');
+const { petsRouter } = require('./api/petsRouter');
 
 const app = express();
 
@@ -11,5 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => res.json('OK'));
+
+// Routes
+app.use('/api/', petsRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
