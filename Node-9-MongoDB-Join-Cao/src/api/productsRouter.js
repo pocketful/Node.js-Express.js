2c6b706e7fe8a491962b412b85c4c650d36742aa
pocketful <iveta.jac@gmail.com>
2022-04-19 19:34:06 +0300

    aggregation, categories prices sums

--- Node-9-MongoDB-Join-Cao/src/api/productsRouter.js
+++ Node-9-MongoDB-Join-Cao/src/api/productsRouter.js
@@ -5,7 +5,7 @@ const productsRouter = express.Router();
 const dbName = 'technologies';
 const collName = 'products';
 
-// GET
+// GET // paduos visus produktus su jų kategorijom (t.y. pavadinimas, kaina, kategorija - pvz. iPhone 7, 370.00, phones).
 productsRouter.get('/products', async (req, res) => {
   try {
     await dbClient.connect();
@@ -16,6 +16,64 @@ productsRouter.get('/products', async (req, res) => {
     res.json(productsArr);
   } catch (error) {
     console.error('error in get products', error);
+    res.status(500).json('something went wrong');
+  } finally {
+    await dbClient.close();
+    console.log('connection closed');
+  }
+});
+
+// GET /categoryvalue/, šis paduos kiekvienos kategorijos produktų kainos sumą
+productsRouter.get('/categoryvalue', async (req, res) => {
+  try {
+    await dbClient.connect();
+    console.log('connection opened');
+    const resource = dbClient.db(dbName).collection(collName);
+    const catValArr = await resource
+      .aggregate([
+        // kiekvienos kategorijos produktų kainos sumą
+        { $match: {} },
+        { $group: { _id: '$category', total: { $sum: '$price' } } },
+        // kiekvienos kategorijos produktų vidutine kaina
+        // { $group: { _id: '$category', average: { $avg: '$price' } } },
+        // { $sort: { price: -1 } },
+      ])
+      .toArray();
+
+    // reduce way
+    const catSumArr = catValArr.reduce((newObj, catObj) => {
+      // eslint-disable-next-line no-underscore-dangle, no-param-reassign
+      newObj[catObj._id] = catObj.total;
+      return newObj;
+    }, {});
+    /*
+    {
+      "phones": 620.5,
+      "tv": 3499.99
+    }
+    */
+
+    // map way
+    // const catSumArr = catValArr.map((catObj) => ({
+    //   // eslint-disable-next-line no-underscore-dangle
+    //   [catObj._id]: catObj.total,
+    // }));
+    /*
+    [
+      {
+        phones: 620.5,
+      },
+      {
+        tv: 3499.99,
+      },
+    ];
+    */
+    console.log(catSumArr);
+    res.json(catSumArr);
+    // console.log(catValArr);
+    // res.json(catValArr);
+  } catch (error) {
+    console.error('error in get categories', error);
     res.status(500).json('something went wrong');
   } finally {
     await dbClient.close();
