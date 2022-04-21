76d0912f150638be12831233ac2f7e0d0c0530ff
pocketful <iveta.jac@gmail.com>
2022-04-21 10:49:03 +0300

    memberships server running

---
+++ Node-10-Memberships/src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const { PORT } = require('./config');
+
+const app = express();
+
+// Middleware
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.json('OK'));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
