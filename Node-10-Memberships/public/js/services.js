b83e561a299097ecb45beb892bcd48f210ec6bc3
pocketful <iveta.jac@gmail.com>
2022-04-21 21:22:19 +0300

    delete service from FE

--- Node-10-Memberships/public/js/services.js
+++ Node-10-Memberships/public/js/services.js
@@ -4,7 +4,6 @@ const cardsOutputEl = document.querySelector('.cards-section');
 
 function displayServices(arr) {
   cardsOutputEl.innerHTML = '';
-
   arr.map((serviceObj) => {
     const cardsEl = document.createElement('article');
     cardsEl.classList = 'card';
@@ -27,10 +26,18 @@ function displayServices(arr) {
     const btnDelEl = document.createElement('button');
     btnDelEl.classList.add('card__btn', 'btn-del');
     btnDelEl.innerHTML = '<i class="trash-icon fa fa-trash" aria-hidden="true"></i>';
+    btnDelEl.addEventListener('click', () => deleteService(serviceObj._id));
     cardsEl.append(btnDelEl);
   });
+  // // delete
+  // document.querySelectorAll('.btn-del').forEach((btn) => {
+  //   btn.addEventListener('click', (e) => {
+  //     deleteServices(`services/${e.target.id}`);
+  //   });
+  // });
 }
 
+// GET
 async function getServices() {
   try {
     const resp = await fetch(`${BASE_URL}/services`);
@@ -46,3 +53,25 @@ async function getServices() {
   }
 }
 getServices();
+
+// DELETE
+async function deleteService(id) {
+  const confirmation = confirm('Do you really want to delete it?');
+  if (!confirmation) return;
+  try {
+    const resp = await fetch(`${BASE_URL}/servicesr/${id}`, {
+      method: 'DELETE',
+    });
+    console.log('resp', resp);
+    // fetch does not pass the code to catch block if status is error
+    if (!resp.ok) throw new Error('Something is wrong');
+    const data = await resp.json();
+    console.log('data', data);
+    if (data.acknowledged) {
+      getServices();
+    }
+  } catch (error) {
+    console.warn('error ===', error);
+    console.log('error');
+  }
+}
