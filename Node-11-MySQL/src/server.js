04ca31cf0c41aa020a743e026481597e79d19552
pocketful <iveta.jac@gmail.com>
2022-04-27 10:57:05 +0300

    get posts arr from mysql FE, BE

--- Node-11-MySQL/src/server.js
+++ Node-11-MySQL/src/server.js
@@ -1,16 +1,46 @@
 const express = require('express');
 const morgan = require('morgan');
+const mysql = require('mysql2/promise');
 const cors = require('cors');
 const { PORT } = require('./config');
 
 const app = express();
+
+// create the connection to database
+const dbConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'ca',
+};
 
 // Middleware
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => res.json('OK'));
+app.get('/api/posts', async (req, res) => {
+  let connection;
+  try {
+    connection = await mysql.createConnection(dbConfig);
+    console.log('connection opened');
+    const sql = 'SELECT * FROM `posts`';
+    // const result = await connection.query(sql);
+    // const [rows, fields] = await connection.query(sql);
+    const [rows] = await connection.query(sql);
+    console.log('rows ===', rows);
+    res.json(rows);
+    // res.json(result[0]);
+  } catch (error) {
+    console.error('route error', error);
+    res.status(500).json('something went wrong');
+  } finally {
+    if (connection) await connection.end();
+    // connection?.end();
+    // await connection.end();
+    console.log('connection ended');
+  }
+});
 
 // Routes
 
