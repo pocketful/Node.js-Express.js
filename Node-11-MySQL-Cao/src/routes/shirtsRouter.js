6777ca678a9c4f006d8327ccfcdbc4210386abe5
pocketful <iveta.jac@gmail.com>
2022-04-27 17:52:30 +0300

    post shirtsRouter

--- Node-11-MySQL-Cao/src/routes/shirtsRouter.js
+++ Node-11-MySQL-Cao/src/routes/shirtsRouter.js
@@ -19,4 +19,22 @@ shirtsRouter.get('/shirts', async (req, res) => {
   }
 });
 
+shirtsRouter.post('/shirts', async (req, res) => {
+  try {
+    const conn = await mysql.createConnection(dbConfig);
+    console.log('connection opened');
+    const {
+      brand, model, size, price,
+    } = req.body;
+    const sql = 'INSERT INTO shirts (brand, model, size, price) VALUES (?, ?, ?, ?)';
+    const [insertResult] = await conn.execute(sql, [brand, model, size, price]);
+    await conn.end();
+    console.log('connection ended');
+    res.json(insertResult);
+  } catch (error) {
+    console.error('error in getting shirts from DB', error);
+    res.status(500).json('something went wrong');
+  }
+});
+
 module.exports = shirtsRouter;
