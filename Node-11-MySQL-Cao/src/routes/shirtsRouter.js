fd03e02b89faee6d6f005a8a81218e3bd578e3a3
pocketful <iveta.jac@gmail.com>
2022-04-27 18:29:00 +0300

    shirtsRouter optional size param

--- Node-11-MySQL-Cao/src/routes/shirtsRouter.js
+++ Node-11-MySQL-Cao/src/routes/shirtsRouter.js
@@ -4,12 +4,30 @@ const { dbConfig } = require('../config');
 
 const shirtsRouter = express.Router();
 
-shirtsRouter.get('/shirts', async (req, res) => {
+// shirtsRouter.get('/shirts', async (req, res) => {
+//   try {
+//     const conn = await mysql.createConnection(dbConfig);
+//     console.log('connection opened');
+//     const sql = 'SELECT * FROM `shirts` ORDER BY price LIMIT 10';
+//     const [rows] = await conn.query(sql);
+//     await conn.end();
+//     console.log('connection ended');
+//     res.json(rows);
+//   } catch (error) {
+//     console.error('error in getting shirts from DB', error);
+//     res.status(500).json('something went wrong');
+//   }
+// });
+
+shirtsRouter.get('/shirts/:size?', async (req, res) => {
   try {
     const conn = await mysql.createConnection(dbConfig);
     console.log('connection opened');
-    const sql = 'SELECT * FROM `shirts`';
-    const [rows] = await conn.query(sql);
+    const { size } = req.params;
+    const sql = size ? 'SELECT * FROM shirts WHERE size = ? ORDER BY price LIMIT 10' : 'SELECT * FROM shirts ORDER BY price LIMIT 10';
+    // const optionalSize = size ? 'WHERE size = ?' : '';
+    // const sql = `SELECT * FROM shirts ${optionalSize} ORDER BY price LIMIT 10`;
+    const [rows] = size ? await conn.execute(sql, [size]) : await conn.query(sql);
     await conn.end();
     console.log('connection ended');
     res.json(rows);
