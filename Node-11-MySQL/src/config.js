ed47d132dc1f630aff7d31d5bd9b14a9f0be1de6
pocketful <iveta.jac@gmail.com>
2022-04-28 10:26:43 +0300

    postsRouter new post, dbconfig to env

--- Node-11-MySQL/src/config.js
+++ Node-11-MySQL/src/config.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
 const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'ca',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
 
 module.exports = {
--- Node-11-MySQL/src/routes/postsRouter.js
+++ Node-11-MySQL/src/routes/postsRouter.js
@@ -5,69 +5,63 @@ const { dbConfig } = require('../config');
 const postsRouter = express.Router();
 
 postsRouter.get('/posts', async (req, res) => {
-  let conn;
   try {
-    conn = await mysql.createConnection(dbConfig);
+    const conn = await mysql.createConnection(dbConfig);
     console.log('connection opened');
     const sql = 'SELECT * FROM `posts`';
     // const result = await conn.query(sql);
     // const [rows, fields] = await conn.query(sql);
     const [rows] = await conn.query(sql);
-    console.log('rows ===', rows);
+    // console.log('rows ===', rows);
+    await conn.end();
+    console.log('connection ended');
     res.json(rows);
     // res.json(result[0]);
   } catch (error) {
     console.error('error in getting posts from DB', error);
     res.status(500).json('something went wrong');
-  } finally {
-    await conn?.end();
-    // if (conn) await conn.end();
-    // await conn.end();
-    console.log('connection ended');
+    // } finally {
+    //   await conn?.end();
+    //   // if (conn) await conn.end();
   }
 });
 
 postsRouter.get('/posts/first-posts', async (req, res) => {
-  let conn;
   try {
-    conn = await mysql.createConnection(dbConfig);
+    const conn = await mysql.createConnection(dbConfig);
     console.log('connection opened');
     const sql = 'SELECT * FROM `posts` LIMIT 2';
     const [rows] = await conn.query(sql);
     console.log('rows ===', rows);
+    await conn.end();
+    console.log('connection ended');
     res.json(rows);
   } catch (error) {
     console.error('error in getting posts from DB', error);
     res.status(500).json('something went wrong');
-  } finally {
-    if (conn) await conn.end();
-    console.log('connection ended');
   }
 });
 
 postsRouter.get('/posts/posts-by-rating', async (req, res) => {
-  let conn;
   try {
-    conn = await mysql.createConnection(dbConfig);
+    const conn = await mysql.createConnection(dbConfig);
     console.log('connection opened');
     const sql = 'SELECT * FROM `posts` ORDER BY rating DESC';
     const [rows] = await conn.query(sql);
     console.log('rows ===', rows);
+    await conn.end();
+    console.log('connection ended');
     res.json(rows);
   } catch (error) {
     console.error('error in getting posts from DB', error);
     res.status(500).json('something went wrong');
-  } finally {
-    if (conn) await conn.end();
-    console.log('connection ended');
   }
 });
 
 // get posts from author dynamic
 postsRouter.get('/posts/author/:author', async (req, res) => {
-  let conn;
   try {
-    conn = await mysql.createConnection(dbConfig);
+    const conn = await mysql.createConnection(dbConfig);
     console.log('connection opened');
     const { author } = req.params;
     // const sql = `SELECT * FROM posts WHERE author = '${author}'`;
@@ -76,35 +70,73 @@ postsRouter.get('/posts/author/:author', async (req, res) => {
     const sql = 'SELECT * FROM posts WHERE author = ?';
     const [rows] = await conn.execute(sql, [author]);
     // console.log('rows ===', rows);
+    await conn.end();
+    console.log('connection ended');
     res.json(rows);
   } catch (error) {
     console.error('error in getting posts from DB', error);
     res.status(500).json('something went wrong');
-  } finally {
-    if (conn) await conn.end();
-    console.log('connection ended');
   }
 });
 
 // get all posts or posts from author? if its included
 postsRouter.get('/posts/author/:author?', async (req, res) => {
-  let conn;
   try {
-    conn = await mysql.createConnection(dbConfig);
+    const conn = await mysql.createConnection(dbConfig);
     console.log('connection opened');
     const { author } = req.params;
     // const sql = author ? `SELECT * FROM posts WHERE author = '${author}'` : 'SELECT * FROM posts';
     // const [rows] = await conn.query(sql);
     // Be aware of SQL injection
     const sql = author ? 'SELECT * FROM posts WHERE author = ?' : 'SELECT * FROM posts';
+    // const sql = `SELECT * FROM posts ${author ? 'WHERE author = ?' : ''}`;
     const [rows] = author ? await conn.execute(sql, [author]) : await conn.query(sql);
     // console.log('rows ===', rows);
+    await conn.end();
+    console.log('connection ended');
     res.json(rows);
   } catch (error) {
     console.error('error in getting posts from DB', error);
     res.status(500).json('something went wrong');
+  }
+});
+
+/* POST --------------------------------------------------------------------------------- */
+postsRouter.post('/posts', async (req, res) => {
+  let conn;
+  try {
+    conn = await mysql.createConnection(dbConfig);
+    console.log('connection opened');
+    // eslint-disable-next-line object-curly-newline
+    const { author, title, body, rating } = req.body;
+    const sql = 'INSERT INTO posts (author, title, body, rating) VALUES (?, ?, ?, ?)';
+    const [insertResult] = await conn.execute(sql, [author, title, body, rating]);
+
+    // 1 way Possibility of SQL injection:
+    // const sql = `INSERT INTO posts (author, title, body, rating) VALUES ('Ron', 'Post 12', 'Body of post 12', 1)`;
+    // const [insertResult] = await conn.query(sql);
+
+    // 2 way
+    // const newPost = req.body;
+    // console.log('newPost ===', newPost);
+    // console.log('Object.values(newPost) ===', Object.values(newPost));
+    // const [insertResult] = await conn.execute(sql, Object.values(newPost));
+
+    // 3 way, shorter
+    // const sql = 'INSERT INTO posts SET ?';
+    // const insertResult = await conn.query(sql, newPost); // const insertResult = await conn.execute(sql, [newPost]); // NOT WORKIN'
+
+    if (insertResult.affectedRows === 1) {
+      console.log(insertResult);
+      res.status(201).json(insertResult);
+      return;
+    }
+    throw new Error('affected row not 1');
+  } catch (error) {
+    console.error('error in creating new post', error);
+    res.sendStatus(500);
   } finally {
-    if (conn) await conn.end();
+    await conn?.end(); // if (conn) await conn.end();
     console.log('connection ended');
   }
 });
