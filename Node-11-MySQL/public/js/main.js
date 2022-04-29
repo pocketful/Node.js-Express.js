d2d4f005173512db2a2f32ad9cb23e9d26883ea6
pocketful <iveta.jac@gmail.com>
2022-04-29 09:05:05 +0300

    post from FE

--- Node-11-MySQL/public/js/main.js
+++ Node-11-MySQL/public/js/main.js
@@ -2,9 +2,35 @@ async function getPosts() {
   const resp = await fetch('http://localhost:3000/api/posts');
   const postsArr = await resp.json();
   console.log('postsArr ===', postsArr);
+  // eslint-disable-next-line no-use-before-define
   makeList(postsArr, 'post-list');
 }
 getPosts();
+
+// eslint-disable-next-line no-unused-vars
+async function createPost(author, title, body, rating) {
+  const newPostObj = {
+    author,
+    title,
+    body,
+    rating,
+  };
+  // console.log('newPostObj ===', newPostObj);
+  try {
+    const resp = await fetch('http://localhost:3000/api/posts/posts-by-rating?order=ASC&limit=5', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(newPostObj),
+    });
+    console.log('response ===', resp);
+    const data = await resp.json();
+    console.log('data ===', data);
+  } catch (error) {
+    console.log('error ===', error);
+  }
+}
 
 async function deletePost(delId) {
   const resp = await fetch(`http://localhost:3000/api/posts/${delId}`, {
