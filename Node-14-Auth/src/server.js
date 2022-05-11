b76458da63d68f2a9a146d45b5dd8af4f74c7577
pocketful <iveta.jac@gmail.com>
2022-05-11 09:49:56 +0300

    users temp arr, post get, login, middleware helper

--- Node-14-Auth/src/server.js
+++ Node-14-Auth/src/server.js
@@ -3,14 +3,73 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const PORT = 3000;
+
 const app = express();
+
+// temp db
+const users = [
+  { id: 1, email: 'hermione@email.com', password: '123456' },
+  { id: 2, email: 'harry@email.com', password: '654321' },
+];
 
 // Middleware
 app.use(morgan('dev'));
+app.use(express.json());
 app.use(cors());
+app.use(showBody);
 
-app.get('/', async (req, res) => {
+// Middleware helper showBody
+function showBody(req, res, next) {
+  if (req.method === 'POST') {
+    console.log('req.body ===', req.body);
+  }
+  next();
+}
+
+// app.post('*', (req, res, next) => {
+//   console.log('req.body ===', req.body);
+//   next();
+// });
+
+app.get('/', (req, res) => {
   res.json('ok');
+});
+
+app.post('/login', (req, res) => {
+  const emailReceived = req.body.email;
+  const passwordReceived = req.body.password;
+  // check if email exists
+  const foundUser = users.find((userObj) => userObj.email === emailReceived);
+  if (!foundUser) {
+    res.status(400).json({ error: 'Email or password not found (test:email)' });
+    return;
+  }
+  // if exists then check if passwords match
+  if (foundUser.password !== passwordReceived) {
+    res.status(400).json({ error: 'Email or password not found (test:pass)' });
+    return;
+  }
+  res.json('login success');
+});
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+// app.post('/register', showBody, (req, res) => { // if with showBody
+app.post('/register', (req, res) => {
+  const { email, password } = req.body;
+  const newUser = {
+    email,
+    password,
+  };
+  users.push(newUser);
+  res.status(201).json(newUser);
+  console.log('users ===', users);
+});
+
+app.all('*', (req, res) => {
+  res.status(404).json({ error: 'Route not found' });
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
