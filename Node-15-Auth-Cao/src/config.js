56b728cc0f117c4ba6612924b05c679403d7d082
pocketful <iveta.jac@gmail.com>
2022-05-17 09:44:04 +0300

    server started

---
+++ Node-15-Auth-Cao/src/config.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+module.exports = {
+  PORT,
+  dbConfig,
+};
