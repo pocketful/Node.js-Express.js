8c965516f28692b30e6f4e849629e167562ba3d1
pocketful <iveta.jac@gmail.com>
2022-05-18 11:20:21 +0300

    regsiter FE, feedback

---
+++ Node-15-Auth-Cao/public/js/modules/feedback.js
@@ -0,0 +1,51 @@
+function clearErrSpans() {
+  document.querySelectorAll('.err').forEach((errEl) => {
+    const error = errEl;
+    error.innerHTML = '';
+    error.previousElementSibling.style.border = 'none';
+  });
+}
+
+function clearErrCommon(output) {
+  const errorCommon = document.getElementById(output);
+  errorCommon.innerHTML = '';
+  return errorCommon;
+}
+
+export function feedback(output, message) {
+  clearErrSpans();
+  const errorCommon = clearErrCommon(output);
+
+  if (typeof message === 'string') {
+    errorCommon.textContent = message;
+  }
+
+  if (Array.isArray(message)) {
+    message.forEach((errObj) => {
+      document.getElementById(errObj.path).style.border = '2px solid red';
+      document.getElementById(errObj.path).nextElementSibling.textContent = errObj.message;
+      // const input = document.querySelector(`#${form} > * > #${errObj.path}`); // #formLogin > * > #email
+    });
+  }
+}
+
+/* passwords --------------------------------------------------------------------- */
+const passInpEl = document.getElementById('password');
+const pass2InpEl = document.getElementById('password2');
+
+export function clearFeedback(output) {
+  // clearErrSpans();
+  clearErrCommon(output);
+  passInpEl.style.border = 'none';
+  pass2InpEl.style.border = 'none';
+  passInpEl.nextElementSibling.textContent = '';
+  pass2InpEl.nextElementSibling.textContent = '';
+}
+
+export function passwordFeedback() {
+  passInpEl.style.border = '2px solid red';
+  pass2InpEl.style.border = '2px solid red';
+  passInpEl.nextElementSibling.textContent = "Passwords does't match";
+  pass2InpEl.nextElementSibling.textContent = "Passwords does't match";
+}
+
---
+++ Node-15-Auth-Cao/public/js/register.js
@@ -0,0 +1,45 @@
+import { feedback, clearFeedback, passwordFeedback } from './modules/feedback.js';
+import { BASE_URL } from './modules/fetch.js';
+
+const { formRegister } = document.forms;
+
+async function registerUser(newRegisterObj) {
+  console.log('newRegisterObj ===', newRegisterObj);
+  try {
+    const resp = await fetch(`${BASE_URL}/register`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(newRegisterObj),
+    });
+    // console.log('response ===', resp);
+    const data = await resp.json();
+    console.log('data ===', data);
+    if (data.success) {
+      formRegister.reset();
+      console.log('registered successfully');
+      feedback('feedbackReg', data.message);
+      // eslint-disable-next-line no-return-assign
+      setTimeout(() => (window.location.href = 'login.html'), 2500);
+    }
+    feedback('feedbackReg', data.message);
+  } catch (err) {
+    console.log('error ===', err);
+  }
+}
+
+formRegister.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const newRegisterObj = {
+    email: formRegister.email.value.trim(),
+    password: formRegister.password.value.trim(),
+    password2: formRegister.password2.value.trim(),
+  };
+  clearFeedback('feedbackReg');
+  if (newRegisterObj.password === newRegisterObj.password2) {
+    registerUser(newRegisterObj);
+  } else {
+    passwordFeedback();
+  }
+});
