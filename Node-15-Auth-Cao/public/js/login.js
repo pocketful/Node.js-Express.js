cc993ea3af73bdc72828be96ac27f9b10afa2935
pocketful <iveta.jac@gmail.com>
2022-05-18 11:21:00 +0300

    login FE, style

---
+++ Node-15-Auth-Cao/public/js/login.js
@@ -0,0 +1,44 @@
+import { feedback } from './modules/feedback.js';
+import { BASE_URL } from './modules/fetch.js';
+
+const { formLogin } = document.forms;
+
+async function loginUser(newLoginObj) {
+  console.log('newLoginObj ===', newLoginObj);
+  try {
+    const resp = await fetch(`${BASE_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(newLoginObj),
+    });
+    console.log('resp ===', resp);
+    const data = await resp.json();
+    console.log('data ===', data);
+    // res.json({ success: true, message: 'Login success.', token });
+    if (data.success) {
+      // save token to localStorage
+      localStorage.setItem('userToken', data.token);
+
+      feedback('feedbackLog', data.message);
+      formLogin.reset();
+
+      // eslint-disable-next-line no-return-assign
+      setTimeout(() => (window.location.href = 'index.html'), 2500);
+    } else {
+      feedback('feedbackLog', data.message);
+    }
+  } catch (err) {
+    console.log('error ===', err);
+  }
+}
+
+formLogin.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const newLoginObj = {
+    email: formLogin.email.value.trim(),
+    password: formLogin.password.value.trim(),
+  };
+  loginUser(newLoginObj);
+});
