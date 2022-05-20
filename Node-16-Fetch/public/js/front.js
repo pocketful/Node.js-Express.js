5a378392011ff3e8d0b1dd2b14eed2383a2ae2fb
pocketful <iveta.jac@gmail.com>
2022-05-20 11:19:38 +0300

    get joke in FE

--- Node-16-Fetch/public/js/front.js
+++ Node-16-Fetch/public/js/front.js
@@ -1,1 +1,34 @@
-console.log('test');
+const BASE_URL = 'http://localhost:3000/api';
+
+const jokeOutputEl = document.getElementById('output');
+
+function showJoke(joke, output) {
+  const outputEl = output;
+  outputEl.textContent = '';
+  outputEl.textContent = joke;
+}
+
+async function getJoke(callback) {
+  try {
+    const resp = await fetch(`${BASE_URL}/joke`);
+    const data = await resp.json();
+    console.log('data:', data);
+    callback(data.joke, jokeOutputEl);
+  } catch (err) {
+    console.log('err:', err);
+  }
+}
+getJoke(showJoke);
+
+// simple, without callback
+// async function getJoke() {
+//   try {
+//     const resp = await fetch(`${BASE_URL}/joke`);
+//     const data = await resp.json();
+//     console.log('data:', data);
+//     showJoke(data.joke, jokeOutputEl);
+//   } catch (err) {
+//     console.log('err:', err);
+//   }
+// }
+// getJoke();
