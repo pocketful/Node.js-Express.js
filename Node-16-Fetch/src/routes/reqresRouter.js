d7c9af1c79117b7469f16491a8cc6d68377ef105
pocketful <iveta.jac@gmail.com>
2022-05-20 11:47:11 +0300

    axios post

--- Node-16-Fetch/src/routes/reqresRouter.js
+++ Node-16-Fetch/src/routes/reqresRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios').default;
 
 const reqresRouter = express.Router();
 
@@ -18,6 +19,29 @@ reqresRouter.post('/create-user', async (req, res) => {
     });
     const data = await resp.json();
     console.log('data ===', data);
+    return res.json({ success: true, message: 'New user added successfully.' });
+  } catch (err) {
+    console.log('err:', err);
+    return res.status(500).json({ success: false, message: 'Something went wrong.' });
+  }
+});
+
+reqresRouter.post('/create-user-axios', async (req, res) => {
+  const newPostObj = {
+    name: req.body.name,
+    job: req.body.occupation,
+  };
+  try {
+    // const data = await resp.json();
+    const resp = await axios.post('https://reqres.in/api/users', {
+      // { data }
+      // method: 'POST',
+      // headers: { 'Content-Type': 'application/json' },
+      // body: JSON.stringify(newPostObj),
+      newPostObj,
+    });
+    // console.log('resp ===', resp);
+    console.log('data ===', resp.data);
     return res.json({ success: true, message: 'New user added successfully.' });
   } catch (err) {
     console.log('err:', err);
