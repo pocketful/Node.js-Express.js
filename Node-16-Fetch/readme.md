82a1e2a55de057081127bf3cc93394712a2d2b13
pocketful <iveta.jac@gmail.com>
2022-05-20 09:55:22 +0300

    node fetch, get noris joke

---
+++ Node-16-Fetch/readme.md
@@ -0,0 +1,3 @@
+## Node Fetch
+[node fetch](https://www.npmjs.com/package/node-fetch)
+- npm install node-fetch
--- Node-16-Fetch/src/routes/norisRouter.js
+++ Node-16-Fetch/src/routes/norisRouter.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const norisRouter = express.Router();
 
+// eslint-disable-next-line no-shadow
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
 norisRouter.get('/joke', async (req, res) => {
-  res.json('test');
+  try {
+    const response = await fetch('https://api.chucknorris.io/jokes/random');
+    const data = await response.json();
+    const jokeObj = {
+      joke: data.value,
+      icon_url: 'https://assets.chucknorris.host/img/avatar/chuck-norris.png',
+    };
+    res.json(jokeObj);
+  } catch (err) {
+    console.log('err:', err);
+  }
 });
 
 module.exports = norisRouter;
