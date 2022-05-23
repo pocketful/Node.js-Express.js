cbfae3ab635c539560d223d51b5c9028b696ed71
pocketful <iveta.jac@gmail.com>
2022-05-23 10:37:34 +0300

    et default view engine ejs

--- Node-17-SSR/src/readme.md
+++
@@ -1,3 +0,0 @@
-## Embedded JavaScript templates
-[ejs](https://www.npmjs.com/package/ejs)
-- npm install ejs
--- Node-17-SSR/src/server.js
+++ Node-17-SSR/src/server.js
@@ -5,23 +5,34 @@ const { PORT } = require('./config');
 
 const app = express();
 
-// static directory
+// Set default view engine - /views
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+// Static directory
 const staticDir = path.join(__dirname, 'assets');
 app.use(express.static(staticDir));
 
 // Middleware
 app.use(morgan('dev'));
 
+const mainInfo = {
+  title: 'SSR',
+  date: '2022',
+};
+
 app.get('/', (req, res) => {
-  const pathToIndex = path.join(__dirname, 'views', 'index.html');
-  console.log(__dirname);
-  res.sendFile(pathToIndex);
+  res.render('index', mainInfo);
+  // const pathToIndex = path.join(__dirname, 'views', 'index.html');
+  // console.log(__dirname);
+  // res.sendFile(pathToIndex);
 });
 
 app.get('/about', (req, res) => {
-  const pathToIndex = path.join(__dirname, 'views', 'about.html');
-  console.log(__dirname);
-  res.sendFile(pathToIndex);
+  res.render('about');
+  // const pathToIndex = path.join(__dirname, 'views', 'about.html');
+  // console.log(__dirname);
+  // res.sendFile(pathToIndex);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
