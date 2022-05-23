28ea68569c2539eff0876307f4c2c433aea873eb
pocketful <iveta.jac@gmail.com>
2022-05-23 09:45:16 +0300

    static directory

--- Node-17-SSR/src/server.js
+++ Node-17-SSR/src/server.js
@@ -5,11 +5,21 @@ const { PORT } = require('./config');
 
 const app = express();
 
+// static directory
+const staticDir = path.join(__dirname, 'assets');
+app.use(express.static(staticDir));
+
 // Middleware
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   const pathToIndex = path.join(__dirname, 'views', 'index.html');
+  console.log(__dirname);
+  res.sendFile(pathToIndex);
+});
+
+app.get('/about', (req, res) => {
+  const pathToIndex = path.join(__dirname, 'views', 'about.html');
   console.log(__dirname);
   res.sendFile(pathToIndex);
 });
